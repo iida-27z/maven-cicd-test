a69f6c0e115ceba8cdeb2c4081772bdb3fd40cc6
iida <iida.s.27z.h3.mailbox@gmail.com>
2025-11-16 12:36:46 +0900

    ユニットテストのJaCoCoレポートの確認(再)

--- src/test/java/io/github/test/app/presentation/controller/RootControllerTest.java
+++ src/test/java/io/github/test/app/presentation/controller/RootControllerTest.java
@@ -1,18 +1,18 @@
-package io.github.test.app.presentation.controller;
+// package io.github.test.app.presentation.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 
-public class RootControllerTest {
-    @Test
-    void testGetRootPage() {
-        //arrange
-        RootController controller = new RootController();
-        //act
-        String actualViewName = controller.getRootPage();
-        //assert
-        String expected = "pages/index";
-        assertEquals(expected, actualViewName);
-    }
-}
+// public class RootControllerTest {
+//     @Test
+//     void testGetRootPage() {
+//         //arrange
+//         RootController controller = new RootController();
+//         //act
+//         String actualViewName = controller.getRootPage();
+//         //assert
+//         String expected = "pages/index";
+//         assertEquals(expected, actualViewName);
+//     }
+// }
